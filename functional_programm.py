3979a822fb43a8dd33b92bd6b757e4e11ecae46d
kdunaev-a10 <ciberkot@gmail.com>
2022-07-18 12:04:22 +0200

    initial push

--- functional_programm.py
+++ functional_programm.py
@@ -1,12 +1,18 @@
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--interfaces', dest='interfaceParce', help='Interface name to change mac' )
-parser.add_argument('-m', '--mac', dest='newmacParce', help='New MAC' )
+def pars():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interfaces', dest='interfaceParse', help='Interface name to change mac' )
+    parser.add_argument('-m', '--mac', dest='newmacParse', help='New MAC' )
+    return parser.parse_args()
 
-options = parser.parse_args()
+def print_function(mac_new, int_name):
+    print(mac_new, int_name)
 
-print(options,type(options), options.newmacParce)
+opt = pars()
+print_function(opt.newmacParse, opt.interfaceParse)
+
+
 
 #subprocess.call("sudo ifconfig", shell=True)
